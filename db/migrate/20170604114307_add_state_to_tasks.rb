0026d012b6cfe0fc78e69dbeda8a48f5e34f92e1
Mandy Real <normandy.real@gmail.com>
2017-06-04 21:47:59 +1000

    Added default to task

--- db/migrate/20170604114307_add_state_to_tasks.rb
+++ db/migrate/20170604114307_add_state_to_tasks.rb
@@ -1,5 +1,5 @@
 class AddStateToTasks < ActiveRecord::Migration[5.0]
   def change
-    add_column :tasks, :state, :string
+    add_column :tasks, :state, :string, default: 'open'
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -14,9 +14,9 @@ ActiveRecord::Schema.define(version: 20170604114307) do
 
   create_table "tasks", force: :cascade do |t|
     t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string   "state"
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.string   "state",      default: "open"
   end
 
   create_table "users", force: :cascade do |t|
